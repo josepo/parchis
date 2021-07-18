1a6edb453abbde42c1f3fc52e66aae699a8c083f
josepo <jlpolo@gmail.com>
2021-07-18 17:02:58 +0200

    Solution and project files

---
+++ Parchis/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Parchis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
